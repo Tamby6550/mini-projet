3543f823e142f63d7b4ed5f5301702b6689b3994
tamby6550 <tambyarimisaemit@gmail.com>
2024-03-06 17:30:50 +0300

    installation bundle

--- config/bundles.php
+++ config/bundles.php
@@ -7,5 +7,6 @@ return [
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
-    
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
 ];
